707fdb3ca640f7ea475f57c9bf1330f72262dfb3
Abrash-Official <abrasharshad440@gmail.com>
2025-07-06 14:29:17 +0500

    flask app

--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Main route (example)
-router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Theory of Automata Node.js backend!' });
-});
-
-export default router; 
